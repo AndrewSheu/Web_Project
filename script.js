d46e5a8ae8a2f5a3cdc49db5c04f6760bc4e60ba
AndrewSheu <andrewsheu0617@gmail.com>
2022-07-15 20:56:00 +1000

    main setup

---
+++ script.js
@@ -0,0 +1,12 @@
+const breakfect_name = ["Avo-Toast", "Smoke Salmon Avo", "Photo & P Croquare", "Rendt Benny", "Egg Benny", "Big Breakfect", "Acai Bowl"]
+const breakfect_ingrediate = []
+const breakfect_price = [16, 19, 20.5, 20, 20.5, 24.5, 18]
+
+const lunch_name = ["Terri Noddle", "Frie Chiecken Rice", "Kac Curry", "Class Beef Buger", "Green Curry Noodle/Rice", "Aiocl", "Acai Bowl"]
+const lunch_ingrediate = []
+const lunch_price = []
+
+const drinks_name = ["", "", ""]
+const drinks_size = []
+const drinks_price = []
+
